8f873cdc0c88387761ebb082b501af7ade57d967
Paolo Nunal <PNunal@Arcanys.com>
2020-01-27 15:30:04 +0800

    [START] Project start for Express Server

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+const mysql = require('mysql');
+
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/posts', (req, res) => {
+    console.log("Loading Posts");
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'drake24_sushicode',
+    });
+
+    connection.connect((err) => {
+        if (err) throw err;
+        console.log('Connected!');
+    });
+
+    connection.query('SELECT * FROM wp_posts LIMIT 1', (err, rows) => {
+        if (err) throw err;
+        console.log(rows);
+    });
+
+    connection.end();
+    res.send('Test Post');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
